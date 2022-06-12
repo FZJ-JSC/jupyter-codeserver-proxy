cb403b3683280e94a4cd60bdee9bb07c1ec96c14
Jens Henrik Göbbert <goebbert1@jwlogin06.juwels>
2022-06-12 23:32:43 +0200

    update

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -93,7 +93,7 @@ def setup_codeserver():
         '--auth=none',  # password
         '--disable-telemetry',
         '--disable-update-check',
-        '--socket=codeserver',
+        '--socket',
         #'--bind-addr=0.0.0.0:{port}',
         # '--user-data-dir=<path>',  # default: ~/.local/share/code-server
         # '--config=<path>',  # default: ~/.config/code-server/config.yaml
@@ -106,6 +106,7 @@ def setup_codeserver():
         'environment': {},
         'command': cmd,
         'unix': True,
+        'port': 'codeserver',
         # 'mappath': _codeserver_mappath,
         'absolute_url': False,
         'timeout': 90,
